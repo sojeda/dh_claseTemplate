70dc07af35897a0b4f2f3b902e69bb32afa1e81e
nick@digitalhouse.com <nick@digitalhouse.com>
2018-05-30 21:18:10 -0300

    App Service Provider Emoji Error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        App::setLocale('es');
+        Schema::defaultStringLength(191);
     }
 
     /**
